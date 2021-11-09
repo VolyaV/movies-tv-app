339c337c737eaf5e2322f601491ba95748ba8736
Volia Vladimir <voliavladimir@gmail.com>
2021-11-09 17:48:25 +0200

    project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <span className='header' onClick={()=>window.scroll(0,0)}>🎬Choose Your Movie</span>
+    <span className='header' onClick={()=>window.scroll(0,0)}>🎬Choose Your Movie🎬</span>
   )
 }
 
